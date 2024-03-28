f83a429475635166aa97542fa269a7e75d108858
Asadujjaman <asad.jaman.emon@gmail.com>
2024-03-28 15:31:46 +0600

    database setup intialize

---
+++ model/User.js
@@ -0,0 +1,15 @@
+import mongoose, {Schema} from 'mongoose';
+
+const dataObject = {
+    username: String,
+    profilePicture: String,
+    phoneNumber: String,
+    birthdate: Date,
+    description: String,
+    activeStatus: Boolean
+}
+
+const userSchema = new Schema(dataObject, { timestamps: true });
+const User = mongoose.model.User ?? mongoose.model('User', userSchema);
+
+export default User;
