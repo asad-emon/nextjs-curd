264543425bd658b6d85ddf0a945672f8ff807bea
Md. Asadujjaman <asad.jaman.emon@gmail.com>
2024-03-29 11:59:20 +0600

    git fix

--- src/components/ui/user-list.js
+++ src/components/ui/user-list.js
@@ -1,6 +1,6 @@
 export function UserList ({children}) {
     return (
-        <ul className="bg-white overflow-hidden max-w-lg mx-auto">
+        <ul className="bg-white rounded-lg shadow overflow-hidden max-w-lg mx-auto dark:bg-gray-800 dark:border-gray-700">
             {children}
         </ul>
     ) 
